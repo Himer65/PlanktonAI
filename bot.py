0158f8759c85c48acccf20a0ea30055d6054786e
Himer65 <nurikmeskov@gmail.com>
2026-07-10 23:42:12 +0700

    add new arhc fft

--- bot.py
+++ bot.py
@@ -2,79 +2,24 @@ import torch
 from torch import nn
 
 
-class EncoderBoard(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(12, 8, 1, bias=True, dtype=torch.float64),
-            nn.ReLU(),
-            nn.Conv2d(8, 8, 2, bias=True, dtype=torch.float64),
-            nn.Conv2d(8, 8, 2, bias=True, dtype=torch.float64),
-            nn.ReLU(),
-            nn.Conv2d(8, 8, 2, bias=True, dtype=torch.float64),
-            nn.Conv2d(8, 8, 2, bias=True, dtype=torch.float64),
-            nn.ReLU(),
-            nn.Conv2d(8, 4, 1, bias=True, dtype=torch.float64),
-            nn.Softsign(),
-        )
-
-    def forward(self, board):
-        x = self.conv(board)
-
-        return x
-
-
-class DecoderBoard(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv = nn.Sequential(
-            nn.ConvTranspose2d(4, 8, 1, bias=True, dtype=torch.float64),
-            nn.ReLU(),
-            nn.ConvTranspose2d(8, 8, 2, bias=True, dtype=torch.float64),
-            nn.ConvTranspose2d(8, 8, 2, bias=True, dtype=torch.float64),
-            nn.ReLU(),
-            nn.ConvTranspose2d(8, 8, 2, bias=True, dtype=torch.float64),
-            nn.ConvTranspose2d(8, 8, 2, bias=True, dtype=torch.float64),
-            nn.ReLU(),
-            nn.ConvTranspose2d(8, 12, 1, bias=True, dtype=torch.float64),
-            nn.Sigmoid(),
-        )
-
-    def forward(self, board):
-        x = self.conv(board)
-
-        return x
-
-
 class Plankton(nn.Module):
-    def __init__(self, enc: EncoderBoard):
+    def __init__(self):
         super().__init__()
-        enc.eval()
-        self.enc = enc
-        self.fc = nn.Sequential(
-            nn.Linear(69, 32, bias=True, dtype=torch.float64),  # Вход 64, +1 за правило 50 ходов,  +2 право на рокировку, +2 случайные числа
+        self.brain = nn.Sequential(
+            nn.Linear(69, 32, bias=True, dtype=torch.float64),  # Вход 64, +1 за правило 50 ходов,  +2 за право на рокировку, +2 случайные числа
             nn.ReLU(),
             nn.Linear(32, 32, bias=True, dtype=torch.float64),
             nn.ReLU(),
-            nn.Linear(32, 32, bias=True, dtype=torch.float64),
+            nn.Linear(32, 64, bias=True, dtype=torch.float64),
             nn.ReLU(),
-            nn.Linear(32, 32, bias=True, dtype=torch.float64),
-            nn.Unflatten(-1, (4, 8)),
-            nn.LogSoftmax(-1),
         )
+        self.from_x = nn.Linear(64, 8, bias=True, dtype=torch.float64)#logsoftmax
+        self.from_y = nn.Linear(64, 8, bias=True, dtype=torch.float64)
+        self.to_x = nn.Linear(64, 8, bias=True, dtype=torch.float64)
+        self.to_y = nn.Linear(64, 8, bias=True, dtype=torch.float64)
 
     def forward(self, board, context):
-        with torch.no_grad():
-            x = self.enc(board)
-            x = torch.flatten(x, start_dim=1)
-
-        x = torch.cat((x, context), -1)
-        x = self.fc(x)
+        x = torch.cat((board, context), -1)
+        x = self.brain(x)
 
         return x
-    
-    def train(self, mode = True):
-        return self.fc.train(mode)
-    
-    def parameters(self, recurse = True):
-        return self.fc.parameters(recurse)
--- dataset.py
+++ dataset.py
@@ -1,4 +1,46 @@
 from torch.utils.data import Dataset
+import torch
+import random
 
 
-class ChessBoardDataset(Dataset): pass
+piece_to_channel = {
+    "P": 0, "N": 1, "B": 2, "R": 3, "Q":  4, "K":  5,  # Белые
+    "p": 6, "n": 7, "b": 8, "r": 9, "q": 10, "k": 11,  # Черные
+}
+piece_max_num = {
+    "P": 8, "N": 2, "B": 2, "R": 2, "Q": 1, "K": 1,  # Белые
+    "p": 8, "n": 2, "b": 2, "r": 2, "q": 1, "k": 1,  # Черные
+}
+
+class ChessDataset(Dataset):
+    def __init__(self, size, dtype = torch.float64, device = "cpu"):
+        self.size = size
+        self.dtype = dtype
+        self.device = device
+
+    def __len__(self):
+        return self.size
+    
+    def __getitem__(self, index):
+        board = [] 
+        rand_pos = self._random_position()
+
+        for piece in ("P", "N", "B", "R", "Q", "K", "p", "n", "b", "r", "q", "k"):
+            piece_board = torch.zeros(8, 8, dtype=self.dtype, device=self.device)
+
+            for _ in range(random.randint(0, piece_max_num[piece])):
+                x, y = rand_pos.pop()
+                piece_board[x, y] = 1.0
+
+            board.append(piece_board)
+
+        board = torch.stack(board)
+
+        return board, board
+
+
+    def _random_position(self):
+        pos = [(x, y) for x in range(8) for y in range(8)]
+        random.shuffle(pos)
+
+        return pos
